c9ada9f9df59b01b4be336a7a613bde9227e52c3
Grubs <ajgibb89@gmail.com>
2023-02-10 05:03:36 -0600

    added border, gravity, and did a little refactoring

--- C++ fundamentals Game Programming For Beginners/axegame/axegame.cpp
+++ C++ fundamentals Game Programming For Beginners/axegame/axegame.cpp
@@ -7,11 +7,19 @@ int main()
     int height{1200};
     InitWindow(width,height, "Grub Window");
 
-    // circle cordinates
-    int circle_x{174};
+    // circle coordinates
+    int circle_x{175};
     int circle_y{100};
     int circle_size{50};
-    int speed{10};
+    int speed{5};
+    int gravity{2};
+    
+    // axe coordinates
+    int axe_x{150};
+    int axe_y{100};
+    int axe_width{50};
+    int axe_height{100};
+    
 
     SetTargetFPS(60);
     // canvas initial drawing
@@ -20,26 +28,32 @@ int main()
         BeginDrawing();
         ClearBackground(WHITE);
 
+        // axe gravity
+        axe_y += gravity;
 
         // Game Logic Begins
         DrawCircle(circle_x,circle_y,circle_size,BLUE);
+        DrawRectangle(axe_x,axe_y,axe_width,axe_height,RED);
 
-        if(IsKeyDown(KEY_D))
+        if(IsKeyDown(KEY_D) && circle_x < width - circle_size)
         {
-            circle_x = circle_x + speed;            
+            circle_x += speed;   
+            axe_x += speed;         
         }
-        if(IsKeyDown(KEY_A))
+        if(IsKeyDown(KEY_A) && circle_x > 0 + circle_size)
         {
-            circle_x = circle_x - speed;
+            circle_x -= speed;
+            axe_x -= speed;
         }
-        if(IsKeyDown(KEY_W))
+        if(IsKeyDown(KEY_W) && circle_y > 0 + circle_size)
         {
-            circle_y = circle_y - speed;
+            circle_y -= speed;
         }
-        if(IsKeyDown(KEY_S))
+        if(IsKeyDown(KEY_S) && circle_y < height - circle_size)
         {
-            circle_y = circle_y + speed;
+            circle_y += speed;
         }
+
 
         // Game Logic Ends
         EndDrawing();
